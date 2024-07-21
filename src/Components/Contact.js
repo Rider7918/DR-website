0f6e5539f8a7705dcb75e4b38ee869c358a3ee51
Rider7918 <ghostrider7918@gmail.com>
2024-07-21 23:37:29 +0530

    insta logo changed

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -5,6 +5,7 @@ import "../Styles/BookAppointment.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { AiFillInstagram } from "react-icons/ai";
+import { RiInstagramFill } from "react-icons/ri";
 import {
   faCircle,
   faCircleCheck,
@@ -41,7 +42,7 @@ function Contact() {
               Medical Stores, Malbhat, Madgaon, Goa 403601{" "}
             </p>
             <p className="ba-checks">
-            <FontAwesomeIcon icon={faInstagram} style={{ color: "#1E8FFD" }} />{" "}
+            <RiInstagramFill color="#1E8FFD"/>{" "}
               <a className="fontLink" href="https://www.instagram.com/mana.shakti/?igsh=MWM1azhkYm5odHhmNQ%3D%3D" target="_blank" rel="noopener noreferrer">mana.shakti
               </a>
             </p>
